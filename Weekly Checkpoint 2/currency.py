2c591ad7f4afdc40e4c341b3308fba398eed98e7
mjsalaicess <mjsalaicess@gmail.com>
2026-08-18 20:20:50 +0000

    me senti como un no sabo kid

---
+++ Weekly Checkpoint 2/currency.py
@@ -0,0 +1,9 @@
+def main()
+    p = float(input("What do you have left in pesos? "))
+    s = float(input("What do you have left in soles? "))
+    r = float(input("What do you have left in reais? "))
+
+    usd = ((p / 0.00032) + (s / 0.30) + (r / 0.19)50)
+    
+        if __name__=="__main__":
+    main()
