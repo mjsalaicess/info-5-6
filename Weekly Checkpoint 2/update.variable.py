436a90129f8d7c28d7c7b44233757656e70b9359
mjsalaicess <mjsalaicess@gmail.com>
2026-08-19 18:01:42 +0000

    se me borro todo y lo tuve que volver a hacer

---
+++ Weekly Checkpoint 2/update.variable.py
@@ -0,0 +1,32 @@
+def main():
+    # Augmented Assignment Operator
+    score = 10
+    score+=10
+    print(score)
+
+    # Substraction Assignment Operator
+    # x -= y
+    grade = 100
+    grade -= 10
+    print(grade)
+
+    # Multiplication Assignmanet Operator
+    balance = 10
+    balance *= 4
+    print(balance)
+
+    # Division Assignment Operator
+    x = 10
+    x /=5
+    print(int(x))
+
+    # Modulus Assignment Operator
+    a = 10
+    b = 4
+
+    a %= b
+    print(a)
+
+if __name__=="__main__":
+    main()
+
