11c43a5c5de19d70b1a0c5f30225ca8a9b3ff375
mjsalaicess <mjsalaicess@gmail.com>
2026-08-24 14:57:18 +0000

    apperantly i think in C and not in Pyton??? like wym i dont even think

---
+++ Weekly Checkpoint 3/password.py
@@ -0,0 +1,10 @@
+def  main():
+
+    password = 6767
+    attempt = int(input("Type correct password "))
+    if attempt == password:
+        print("Welcome to the Super Secret Aura Farming Battle/FBI V1")
+    else:
+        print("You lost a 1000 aura points, please enter the right password you doofus")
+if __name__=="__main__":
+    main()
