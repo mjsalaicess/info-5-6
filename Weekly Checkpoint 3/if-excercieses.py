665800ea036d6ccd142ae6585d2ad22153c15d7c
mjsalaicess <mjsalaicess@gmail.com>
2026-08-26 18:11:14 +0000

    im not ready for a test

---
+++ Weekly Checkpoint 3/if-excercieses.py
@@ -0,0 +1,14 @@
+def main():
+    number1 = float(input("Type a number:"))
+    operation = input("Type an operation:")
+    number2 = float(input("Type a number:"))
+    if operation == "+":
+        print(number1 +number2 )
+
+    elif opration == "-":
+        print(number1 -number2 )
+
+    #string.split" "
+
+if  __name__=="__main__":
+    main()
