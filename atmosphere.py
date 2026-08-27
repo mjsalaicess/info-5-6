a799500f30d49ad54513d978792ff5062bb4f97e
mjsalaicess <mjsalaicess@gmail.com>
2026-08-27 14:58:49 +0000

    frikin test

---
+++ atmosphere.py
@@ -0,0 +1,20 @@
+def main():
+    atmosphere = input("Descent atmosphere layer:")
+    if atmosphere == "Exosphere":
+        print("Your altitude level will be betweeen 700 and 10,000 km")
+    elif atmosphere == "Thermosphere":
+        print("Your altitude level will be betweeen 85 and 700 km")
+    elif atmosphere == "Mesosphere":
+        print("Your altitude level will be betweeen 50 and 85 km")
+    elif atmosphere == "Stratosphere":
+        print("Your altitude level will be betweeen 12 and 50 km")
+    elif atmosphere == "Troposphere":
+        print("Your altitude level will be betweeen 0 and 10 km")
+    else:
+        print("Invalid. Try again.")
+
+
+
+
+if __name__=="__main__":
+    main()
