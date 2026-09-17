c779cae94b52a0a76cbff01a23ad16eaa1579b38
mjsalaicess <mjsalaicess@gmail.com>
2026-09-17 15:40:33 +0000

    idk what the hai

---
+++ to-do2.py
@@ -0,0 +1,16 @@
+def main():
+    tasks = []
+    while True:
+        print("Tasks to do:{len(tasks)}")
+        print(tasks)
+        new_task = input("Enter task: ").capitalize().strip()
+
+        if new_task == "Exit":
+            break
+        elif new_task not in tasks:
+            tasks.append(new_task)
+        elif new_task in tasks:
+            tasks.remove(new_task)
+            print("Task removed from the list.")
+if __name__=="__main__":
+    main()
