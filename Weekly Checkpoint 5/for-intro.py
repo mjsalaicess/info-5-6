d00b9e88c1c17390b392259ef70e321ae5f84cc0
mjsalaicess <mjsalaicess@gmail.com>
2026-09-18 20:20:39 +0000

    tengo mucho sueñooooooooooooooooooo

---
+++ Weekly Checkpoint 5/for-intro.py
@@ -0,0 +1,7 @@
+def main():
+    doctrine = ["Faith in Jesus Christ", "Reptence", "Baptism", "Confirmation", "Enduring to the end"]
+    for i in doctrine:
+        print(i)
+
+if __name__=="__main__":
+    main()
