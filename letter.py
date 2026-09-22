46154c122666dc525b9b8aba4c377a5f01c791a7
mjsalaicess <mjsalaicess@gmail.com>
2026-09-22 20:12:23 +0000

    Dear profe, You are cordially invited to a ball at Peach's Castle this evening, 7:00 PM.  Sincerely, tired and confused but with a fire playlist.

---
+++ letter.py
@@ -0,0 +1,18 @@
+def main():
+    friends = ["Mario", "Luigi", "Daisy", "Yoshi", "Toad", "Princess Peach", "Bowser", "Toadette", "Wario"]
+
+    for receiver in friends:
+        if receiver != "Princess Peach":
+            print(f"""
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+       Dear {receiver},
+
+       You are cordially invited to a ball at
+       Peach's Castle this evening, 7:00 PM.
+
+       Sincerely,
+       {friends[5]}
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+""")
+if __name__=="__main__":
+    main()
