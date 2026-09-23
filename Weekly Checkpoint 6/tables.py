73acfe6b68e956672925a07a6d8044c3154dc784
mjsalaicess <mjsalaicess@gmail.com>
2026-09-23 17:59:44 +0000

    el profe dice que mi logic no logiquea

--- Weekly Checkpoint 6/tables.py
+++ Weekly Checkpoint 6/tables.py
@@ -1,10 +1,14 @@
 def main():
-    number = int(input("Enter a number(1-10):"))
-    print(f"Here is the {number} times table")
-    for i in range(1,11):
-        result = i * number
-        print (f"{i} times {number} is {result}")
-
+    while True:
+        number = input("Enter a number(1-10):")
+        if number != "exit":
+            number = int(number)
+            print(f"Here is the {number} times table")
+            for i in range(1,11):
+                result = i * number
+                print (f"{i} times {number} is {result}")
+        elif number == "exit":
+            break
 
 if __name__=="__main__":
     main()
